5eb00d4487b2f3db373a38019ccd3a072546586e
vishwajeet7077 <41vishwajeet@gmail.com>
2023-12-13 22:51:17 +0530

    About Page Updated

--- serviceappfront/src/component/uHomePage/About.js
+++ serviceappfront/src/component/uHomePage/About.js
@@ -1,47 +1,33 @@
 import React from "react";
 import "./HomePage.css";
-export const About = (props) => {
+
+export const About = () => {
   return (
     <div id="about" className="font">
       <h3 className="menuheading">ABOUT US</h3>
-      <div className="container mt-5 p-5">
-        <div className="flex justify-around">
-          <div className="col-md-6 d-flex align-items-center m-5">
-            <img src="img/about.jpg" className="img-fluid" alt="About Us" />
+      <div className="container mt-5 p-3 md:p-5">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mb-4 md:mb-0 text-center">
+            <img
+              src="img/about.jpg"
+              className="w-full md:rounded"
+              alt="About Us"
+            />
           </div>
-          <div className="col-md-6 m-5 ">
-            <div className="about-text">
-              <h2 className="font-weight-bold mb-3 text-primary text-4xl">Our Story</h2>
+          <div className="md:w-1/2 md:ml-4">
+            <div className="about-text text-center">
+              <h2 className="font-weight-bold mb-3 text-primary text-4xl">
+                Our Story
+              </h2>
               <p className="text-muted text-xl">
-                {props.data ? props.data.paragraph : "Loading..."}
+                This utility is not just about convenience; it's about empowering local economies and fostering stronger community ties in an increasingly digital age.
               </p>
-              <h3 className="font-weight-bold mt-4 text-success text-xl">
+              <h3 className="font-weight-bold mt-4 text-success text-3xl">
                 Why Choose Us?
               </h3>
-              <div className="row">
-                <div className="col-6">
-                  <ul className="list-unstyled">
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`} className="text-info">
-                            {d}
-                          </li>
-                        ))
-                      : "Loading"}
-                  </ul>
-                </div>
-                <div className="col-6">
-                  <ul className="list-unstyled">
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`} className="text-info">
-                            {d}
-                          </li>
-                        ))
-                      : "Loading"}
-                  </ul>
-                </div>
-              </div>
+              <p className="text-muted text-xl">
+                The amalgamation of cutting-edge technology with a focus on community connections makes our platform exceptionally useful. Users can effortlessly navigate a feature-rich environment, utilizing tools such as location maps, availability checks, and reviews to make informed decisions. Simultaneously, businesses enhance their visibility and reach, enjoying a platform that not only connects them with potential customers but also facilitates the showcasing of their expertise through previous work displays.
+              </p>
             </div>
           </div>
         </div>
@@ -49,4 +35,5 @@ export const About = (props) => {
     </div>
   );
 };
+
 export default About;
